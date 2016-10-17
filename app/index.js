9b95364ad83e8a01a9abbd6a1aa551bcccdb5d36
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 17:00:21 -0500

    loop

--- app/index.js
+++ app/index.js
@@ -2,3 +2,13 @@ import 'whatwg-fetch';
 import './data.json';
 import './track-model';
 import './track-view';
+
+const el = document.createElement('div');
+SpotifyTrack.forEach((item) => {
+  const t = new Track(item);
+
+  const view = new TrackView(t);
+  view.render();
+  document.body.appendChild(view.el);
+
+}
