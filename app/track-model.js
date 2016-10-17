b5f55a4bc78b3f45fdcc0d64bb4c647014ecb583
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 17:33:08 -0500

    style

--- app/track-model.js
+++ app/track-model.js
@@ -3,7 +3,7 @@ export default class Track {
     this.artist = track.artists[0].name;
     this.name = track.name;
     this.image = track.album.images[0].url;
-    this.url = track.external_urls;
+    this.url = track.external_urls.spotify;
 
     console.log(this);
   }
--- app/track-view.js
+++ app/track-view.js
@@ -4,17 +4,21 @@ export default class SpotifyTrack {
 
     this.el = document.createElement('div');
     this.el.classList.add('track');
+    this.el.classList.add('panel-block');
     this.el.innerHTML = `
+      <div class="track__frame">
+        <img class="track__image"/>
+      </div>
       <p class="track__artist"></p>
       <p class="track__name"></p>
       <a class="track__url"></a>
-      <img class="track__image"/>`;
+      `;
   }
 
   render() {
     this.el.querySelector('.track__artist').innerText = this.data.artist;
     this.el.querySelector('.track__name').innerText = this.data.name;
     this.el.querySelector('.track__image').src = this.data.image;
-    this.el.querySelector('.track__url').innerText = this.data.url;
+    this.el.querySelector('.track__url').href = this.data.url;
   }
 }
